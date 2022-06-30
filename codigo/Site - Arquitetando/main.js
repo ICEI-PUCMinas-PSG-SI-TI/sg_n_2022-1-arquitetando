b249a2dceab07ec913eb3c03c735ea18aeeac950
Felipe Cardoso <pcardoso.felipe@gmail.com>
2022-06-29 21:37:07 -0300

    Perfil - Ver mais

--- codigo/Site - Arquitetando/main.js
+++ codigo/Site - Arquitetando/main.js
@@ -33,3 +33,19 @@ function loginUser() {
         console.log('Credenciais erradas') 
     }
 }
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Ver mais";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Ver menos";
+      moreText.style.display = "inline";
+    }
+  }
